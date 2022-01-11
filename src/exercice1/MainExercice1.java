bf4cbdfe6c1ff01b47ecdfe9e2a62c0afbaea55d
StephanKit <skitschminyof@gmail.com>
2022-01-11 08:38:02 +0100

    exo1

--- src/exercice1/MainExercice1.java
+++ src/exercice1/MainExercice1.java
@@ -1,7 +1,9 @@
 package exercice1;
 
 import datamocklib.Person;
+import datamocklib.TxtHelper;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -9,9 +11,20 @@ public class MainExercice1 {
 
     // Format du fichier : firstName,lastName,birthDate,gender,cityOfResidence,cityOfBirth
     // Pas de header
-    public List<Person> parse(List<String> data) {
-        // A COMPLETER
-        return new ArrayList<Person>();
+    public static List<Person> parse(List<String> data) {
+        ArrayList<Person> personnes = new ArrayList<Person>();
+
+        Person p;
+        String[] splited_data;
+
+        for (int i=0; i<data.size(); i++) {
+            splited_data = data.get(i).split(",");
+            p = new Person(splited_data[0], splited_data[1], splited_data[2], splited_data[3], splited_data[4], splited_data[5]);
+
+            personnes.add(p);
+        }
+
+        return personnes;
     }
 
     public static void main(String[] args) {
@@ -23,7 +36,9 @@ public class MainExercice1 {
          * - Calculer le nombre de personnes nées dans chaque ville avec les données obtenues.
          * Les données contiennent une trentaine de villes, Bonne chance !
         */
+        List<Person> personnes = parse(TxtHelper.getDataFromTxt("persons.txt"));
 
 
+        System.out.println(personnes);
     }
 }
